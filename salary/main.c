e8928198dbcdd01477793902d31eea862784dad7
Tejas Bhovad <tejasbhovad@gmail.com>
2022-10-02 21:49:19 +0530

    added first program

---
+++ salary/main.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    double grossSal, baseSal, da, hra;
+    printf("Enter Basic Salary: ");
+    scanf("%lf",&baseSal);
+    da = (50 * baseSal) / 100;
+    hra = (40 * baseSal) / 100;
+    grossSal = baseSal + hra + da;
+    printf("Gross Salary: %.2lf", grossSal);
+    printf("\n");
+}
